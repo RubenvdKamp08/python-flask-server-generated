a4673b63366da014bad17b73f6f9cd60ba7f6765
Ruben van de Kamp <rubenvdkamp08@gmail.com>
2023-02-15 19:12:35 +0100

    add mongodb

--- swagger_server/service/student_service.py
+++ swagger_server/service/student_service.py
@@ -1,41 +1,43 @@
 import os
-import tempfile
-from functools import reduce
+from pymongo import MongoClient
+from bson.objectid import ObjectId
 
-from tinydb import TinyDB, Query
+client = MongoClient(os.environ['MONGO_URI'])
+db = client.local
+collection = db.students
 
-db_dir_path = tempfile.gettempdir()
-db_file_path = os.path.join(db_dir_path, "students.json")
-student_db = TinyDB(db_file_path)
+id = db.info.find_one()
+if not id:
+    db.info.insert_one({ 'id_counter': 0 })
 
+def get_id():
+  id = db.info.find_one()['id_counter'] + 1
+  db.info.update_many({}, { '$set': { 'id_counter': id } })
+  return id
 
 def add(student=None):
-    queries = []
-    query = Query()
-    queries.append(query.first_name == student.first_name)
-    queries.append(query.last_name == student.last_name)
-    query = reduce(lambda a, b: a & b, queries)
-    res = student_db.search(query)
+    res = collection.find_one({ 'first_name': student.first_name, 'last_name': student.last_name })
     if res:
         return 'already exists', 409
 
-    doc_id = student_db.insert(student.to_dict())
-    student.student_id = doc_id
+    student.student_id = get_id()
+    student_dict = student.to_dict()
+    collection.insert_one(student_dict)
+
     return student.student_id
 
 
 def get_by_id(student_id=None, subject=None):
-    student = student_db.get(doc_id=int(student_id))
+    student = collection.find_one({'student_id': student_id})
     if not student:
         return 'not found', 404
-    student['student_id'] = student_id
-    print(student)
+    del student['_id']
     return student
 
 
 def delete(student_id=None):
-    student = student_db.get(doc_id=int(student_id))
-    if not student:
+    _student = collection.find_one({'student_id': student_id})
+    if not _student:
         return 'not found', 404
-    student_db.remove(doc_ids=[int(student_id)])
+    collection.delete_one(({'student_id': student_id}))
     return student_id
